25b3954dddd14faa179a5167e49e910710f0bcbd
Harsh Deep <hdeep61034@gmail.com>
2024-01-02 23:39:06 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-700 text-3xl">HELLO WORLD!</h1>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="border-2 border-black w-96 h-96 rounded-xl">
+          <h1 className="text-center text-3xl font-bold">CHAT APP</h1>
+          <div className="w-full h-full flex justify-center items-center flex-col text-2xl">
+            <input
+              type="text"
+              id="code"
+              name="code"
+              className="bg-slate-300 p-2 rounded-xl"
+            />
+            <button className="px-2 py-1 bg-black text-white rounded-lg my-4 cursor-pointer">
+              Join
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
